d8c5016cb9db0b3b1588fae5dcca827b98102d91
romeros69 <jefopc@gmail.com>
2022-09-17 19:21:25 +0300

    fix: добавил поле дата в сущность shipment

--- internal/entity/shipment.go
+++ internal/entity/shipment.go
@@ -1,7 +1,10 @@
 package entity
 
+import "time"
+
 type Shipment struct {
-	ID          int64 `json:"id"`
-	OrderID     int64 `json:"order_id"`
-	CountryToID int64 `json:"country_to_id"`
+	ID          int64     `json:"id"`
+	OrderID     int64     `json:"order_id"`
+	CountryToID int64     `json:"country_to_id"`
+	Date        time.Time `json:"date"`
 }
