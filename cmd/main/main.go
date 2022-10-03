ccbcdbeb346c92ab20d70a987724d0374e2a4879
Pivnoy <evgenij_makarev20021@mail.ru>
2022-10-03 03:22:14 +0300

    feat: Добавление скелета Swagger

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"log"
 	"pahan/config"
+	_ "pahan/docs"
 	"pahan/internal/app"
 )
 
+// @title Автомобилестроение в экономике
+// @version 1.0
+// @description Курсовая работа по предмету "Информационные системы и базы данных" студента группы P34312, Соловьева Павла
+
+// @host localhost:9000
+// @BasePath /
 func main() {
 	cfg, err := config.NewConfig()
 	if err != nil {
